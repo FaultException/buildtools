7a099ab97df26ea83fbfdf53a7367428919f8451
Daniel Bateman <jetison.24@gmail.com>
2012-05-08 00:52:27 -0500

    fix repo start

--- patch.sh
+++ patch.sh
@@ -12,7 +12,7 @@ repo abandon auto
 
 # Patches
 echo "### aries-common: tenative fix for \"mute-unmute\" bug"
-repo start device/samsung/aries-common
+repo start auto device/samsung/aries-common
 cd device/samsung/aries-common
 git fetch ssh://FaultException@review.cyanogenmod.com:29418/CyanogenMod/android_device_samsung_aries-common refs/changes/20/15720/1 && git cherry-pick FETCH_HEAD
 cd ../../..
