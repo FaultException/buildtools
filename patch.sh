8483d0441f6ee22a295a9b8bdcf7a0f176898461
Daniel Bateman <faultexception@cyanogenmod.org>
2012-12-11 18:08:56 -0600

    update patches
    Change-Id: I5e675f0290808594bd3766900ef8bc52d610f2f5

--- patch.sh
+++ patch.sh
@@ -18,18 +18,6 @@ repo abandon auto
 # <cherry-pick-line>
 # cd ../../..
 
-echo "### aries-common: Jellybean 4.2 bring up"
-repo start auto device/samsung/aries-common
-pushd device/samsung/aries-common
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_aries-common refs/changes/54/26954/5 && git cherry-pick FETCH_HEAD
-popd
-
-echo "### bcmdhd: Enable ad-hoc networks"
-repo start auto kernel/samsung/tuna
-pushd kernel/samsung/tuna
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_kernel_samsung_tuna refs/changes/80/16280/1 && git cherry-pick FETCH_HEAD
-popd
-
 echo "### vold: mount sdcard with UID root"
 repo start auto system/vold
 pushd system/vold
@@ -39,11 +27,23 @@ popd
 echo "### Camera: Allow a device to request the focus modes at build time"
 repo start auto packages/apps/Camera
 pushd packages/apps/Camera
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Camera refs/changes/96/27996/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Camera refs/changes/96/27996/2 && git cherry-pick FETCH_HEAD
 popd
 
 echo "### Clock: fix layout for 320dp devices"
 repo start auto packages/apps/DeskClock
 pushd packages/apps/DeskClock
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_DeskClock refs/changes/01/28001/3 && git cherry-pick FETCH_HEAD
+popd
+
+echo "### build: Update domain name"
+repo start auto build
+pushd build
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/58/28058/1 && git cherry-pick FETCH_HEAD
+popd
+
+echo "### Set ANDROID_PROPERTY_WORKSPACE in exec."
+repo start auto system/core
+pushd build
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/61/28061/1 && git cherry-pick FETCH_HEAD
 popd
